b598eb7ea4e8bdeb842489d573dc8fd612f32768
M-M1318111 <mayanksingh9889659765@gmail.com>
2026-08-27 00:35:35 +0530

    update Vercel deployment setup

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Portal folder mein settings.py hai
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings')
+
 app = get_wsgi_application()

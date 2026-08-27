34b2701bee41351793f8200846a860df0a94f978
M-M1318111 <mayanksingh9889659765@gmail.com>
2026-08-27 10:43:41 +0530

    set email port 587 with tls true and ssl false

--- core/settings.py
+++ core/settings.py
@@ -134,10 +134,10 @@ else:
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "mayanksingh9889659765@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "apualbvhfuzrquzk")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "pwfaclbbxuqlgksy")
 EMAIL_TIMEOUT = 10
 
 DEFAULT_FROM_EMAIL = f"Central Exam Portal <{EMAIL_HOST_USER}>"

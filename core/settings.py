d7a26693ce0c249a15f6a0a751819593c9a9cfac
M-M1318111 <mayanksingh9889659765@gmail.com>
2026-08-27 10:14:59 +0530

    switch email smtp to port 587 tls

--- core/settings.py
+++ core/settings.py
@@ -133,7 +133,7 @@ else:
 # Email Configuration (Gmail SMTP)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "mayanksingh9889659765@gmail.com")
